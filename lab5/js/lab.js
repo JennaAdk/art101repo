e56671c96aca222f9d9021003123a54d468c0764
jennaadkison <jennaadkison@gmail.com>
2021-10-13 11:57:58 -0700

    minor indent fixes

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+  * Author: Jenna Adkison
+  * Created: 10/14/2021
+  * License: Public Domain
+**/
+
+// Define the Variables
+make = "Jeep";
+model = "Wrangler";
+color = "Green";
+year = "1998";
+
+//Calculate
+age = 2021 - year;
+
+//Output
+document.writeln("Make:" + make + "<br>");
+document.writeln("Model:" + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year:" + year + "<br>");
+document.writelm("Age:" + age + "years<br>");
