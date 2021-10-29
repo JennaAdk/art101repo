67819ef83e041a69cde7e436bc793c273bc756df
jennaadkison <jennaadkison@gmail.com>
2021-10-29 12:58:31 -0700

    apple music

---
+++ lab9/js/lab.js
@@ -0,0 +1,9 @@
+/**
+  * Author: Jenna Adkison
+  * Created: 10/14/2021
+  * License: Public Domain
+**/
+
+var outputEl = getElementId("output");
+var new1El = document.createElement("newElement");
+var new2El = document.createElement("new2Element");
