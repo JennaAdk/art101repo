c8c56c590e49b6ee5d79820e746efd48d9686701
jennaadkison <jennaadkison@gmail.com>
2021-10-30 19:42:51 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,9 +1,29 @@
 /**
   * Author: Jenna Adkison
-  * Created: 10/14/2021
+  * Created: 10/30/2021
   * License: Public Domain
 **/
 
-var outputEl = getElementId("output");
-var new1El = document.createElement("newElement");
-var new2El = document.createElement("new2Element");
+
+//referencing to <div> "output"
+var outputEl = document.getElementById("output");
+
+//creating new paragraph elements
+var new1El = document.createElement("p");
+new1El.innerHTML = "Does this thing work?";
+
+var new2El = document.createElement("p");
+new2El.innerHTML ="I think it works!";
+
+// appending the new elements to the <div> "output"
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
+
+// Task X:
+//appending an Element to the top
+var new3El = document.createElement("p");
+new3El.innerHTML = "*cough cough*";
+outputEl.prepend(new3El);
+
+//DOM element -accessKey
+document.getElementById("home").accessKey = "j";
